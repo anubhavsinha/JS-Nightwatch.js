90349460e9c551f6010d3f81a9f74c285fd16f88
Anubhav Sinha <anubhav.sinha@opscale.io>
2017-02-15 23:18:08 +0530

    Update guineaPig.js

--- tests/guineaPig.js
+++ tests/guineaPig.js
@@ -6,7 +6,7 @@ module.exports = {
         client
             .url('https://saucelabs-sample-test-frameworks.github.io/training-test-page')
             .waitForElementVisible('body', 5000)
-            .assert.title('I am a page title - Sauce Labs Error');
+            .assert.title('I am a page title - Sauce Labs');
     },
 
     afterEach: function(client, done) {
